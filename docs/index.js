7e3de81355d231034de55836bd7d7d6ce4742ae5
scsk-nakagome <nakagome@scsk.jp>
2024-08-30 11:12:59 +0900

    initial commit

---
+++ docs/index.js
@@ -0,0 +1,60 @@
+require("./index.css");
+var $kpo7N$reactjsxruntime = require("react/jsx-runtime");
+var $kpo7N$react = require("react");
+var $kpo7N$reactdomclient = require("react-dom/client");
+var $kpo7N$webvitals = require("web-vitals");
+
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+
+
+function $fad3117c681baaa8$var$App({ domElement: domElement }) {
+    return /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsxs)("div", {
+        className: "App",
+        children: [
+            /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)("header", {
+                className: "App-header",
+                children: /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)("p", {
+                    children: domElement
+                })
+            }),
+            /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)("div", {
+                children: /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)("button", {
+                    onClick: console.log("\u767A\u6CE8\u3092\u78BA\u5B9A\u3059\u308B"),
+                    children: "\u767A\u6CE8\u3092\u78BA\u5B9A\u3059\u308B"
+                })
+            })
+        ]
+    });
+}
+var $fad3117c681baaa8$export$2e2bcd8739ae039 = $fad3117c681baaa8$var$App;
+
+
+
+const $5020c1d6ba5e731d$var$reportWebVitals = (onPerfEntry)=>{
+    if (onPerfEntry && onPerfEntry instanceof Function) $5020c1d6ba5e731d$importAsync$c05528d6c23697e1.then(({ getCLS: getCLS, getFID: getFID, getFCP: getFCP, getLCP: getLCP, getTTFB: getTTFB })=>{
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+    });
+};
+var $5020c1d6ba5e731d$export$2e2bcd8739ae039 = $5020c1d6ba5e731d$var$reportWebVitals;
+
+
+const $ac8f80d7ef71e787$var$root = (0, ($parcel$interopDefault($kpo7N$reactdomclient))).createRoot(document.getElementById("root"));
+$ac8f80d7ef71e787$var$root.render(/*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)((0, ($parcel$interopDefault($kpo7N$react))).StrictMode, {
+    children: /*#__PURE__*/ (0, $kpo7N$reactjsxruntime.jsx)((0, $fad3117c681baaa8$export$2e2bcd8739ae039), {})
+}));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+(0, $5020c1d6ba5e731d$export$2e2bcd8739ae039)();
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function App({ domElement }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{domElement}</p>
       </header>
+      <div>
+        <button onClick={console.log("発注を確定する")}>発注を確定する</button>
+      </div>
     </div>
   );
 }
